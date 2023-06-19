c0c3cfc9ddedef75e53f28cdadb54837cbd725f8
tangq <tangqianyu123456@gmail.com>
2023-06-19 09:59:02 +0800

    no message

--- src/app/app.setting.ts
+++ src/app/app.setting.ts
@@ -1,3 +1,2 @@
-export const REMOVE_BG_API_KEY = 'YOUR_API_KEY';
-
-export const RAPID_API_KEY = 'YOUR_API_KEY';
+export const REMOVE_BG_API_KEY = 'UgnB6Xish6nSEvEM4TjDfCCa';
+export const RAPID_API_KEY = '2a87c5a13cmsh98ac381d103b957p1e389fjsn09ff3259f440';
