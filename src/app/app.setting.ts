f84f254df54a3165786c8bf76b96477ec63f0e6f
tangq <tangqianyu123456@gmail.com>
2023-06-19 21:43:52 +0800

    no message

--- src/app/app.setting.ts
+++ src/app/app.setting.ts
@@ -1,6 +1,6 @@
-export const REMOVE_BG_API_KEY = 'UgnB6Xish6nSEvEM4TjDfCCa';
+export const REMOVE_BG_API_KEY = 'YOUR_API_KEY';
 
-export const RAPID_API_KEY = '2a87c5a13cmsh98ac381d103b957p1e389fjsn09ff3259f440';
+export const RAPID_API_KEY = 'YOUR_API_KEY';
 
-export const PHOTO_ROOM_API_KEY = 'f5ec330a8f2115042d0b3da425abfd430fd0e87c';
+export const PHOTO_ROOM_API_KEY = 'YOUR_API_KEY';
 
